34049e158c3fa310e6000885cf5f7b27641a0029
Jernej Skrabec <jernej.skrabec@gmail.com>
2024-05-18 17:10:36 +0200

    properly fix priorities
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- arch/arm/mach-sunxi/dram_sun50i_h616.c
+++ arch/arm/mach-sunxi/dram_sun50i_h616.c
@@ -55,8 +55,8 @@ static void mbus_configure_port(u8 port,
 	writel_relaxed(cfg1, &mctl_com->master[port].cfg1);
 }
 
-#define MBUS_CONF(port, bwlimit, priority, qos, acs, bwl0, bwl1, bwl2)	\
-	mbus_configure_port(port, bwlimit, priority, \
+#define MBUS_CONF(port, priority, qos, acs, bwl0, bwl1, bwl2)	\
+	mbus_configure_port(port, true, priority, \
 			    MBUS_QOS_ ## qos, 0, acs, bwl0, bwl1, bwl2)
 
 static void mctl_set_master_priority(void)
@@ -68,25 +68,25 @@ static void mctl_set_master_priority(void)
 	writel(399, &mctl_com->tmr);
 	writel(BIT(16), &mctl_com->bwcr);
 
-	MBUS_CONF( 0, false, true, HIGHEST, 0,  256,  128,  100);
-	MBUS_CONF( 1, false, true,    HIGH, 0, 1536, 1400,  256);
-	MBUS_CONF( 2, false, true, HIGHEST, 0,  512,  256,   96);
-	MBUS_CONF( 3, false, true,    HIGH, 0,  256,  100,   80);
-	MBUS_CONF( 4, false, true,    HIGH, 2, 8192, 5500, 5000);
-	MBUS_CONF( 5, false, true,    HIGH, 2,  100,   64,   32);
-	MBUS_CONF( 6, false, true,    HIGH, 2,  100,   64,   32);
-	MBUS_CONF( 8, false, true,    HIGH, 0,  256,  128,   64);
-	MBUS_CONF(11, false, true,    HIGH, 0,  256,  128,  100);
-	MBUS_CONF(14, false, true,    HIGH, 0, 1024,  256,   64);
-	MBUS_CONF(16, false, true, HIGHEST, 6, 8192, 2800, 2400);
-	MBUS_CONF(21, false, true, HIGHEST, 6, 2048,  768,  512);
-	MBUS_CONF(22, false, true,    HIGH, 0,  256,  128,  100);
-	MBUS_CONF(25,  true, true, HIGHEST, 0,  100,   64,   32);
-	MBUS_CONF(26, false, true,    HIGH, 2, 8192, 5500, 5000);
-	MBUS_CONF(37, false, true,    HIGH, 0,  256,  128,   64);
-	MBUS_CONF(38, false, true,    HIGH, 2,  100,   64,   32);
-	MBUS_CONF(39, false, true,    HIGH, 2, 8192, 5500, 5000);
-	MBUS_CONF(40, false, true,    HIGH, 2,  100,   64,   32);
+	MBUS_CONF( 0, false, HIGHEST, 0,  256,  128,  100);
+	MBUS_CONF( 1, false,    HIGH, 0, 1536, 1400,  256);
+	MBUS_CONF( 2, false, HIGHEST, 0,  512,  256,   96);
+	MBUS_CONF( 3, false,    HIGH, 0,  256,  100,   80);
+	MBUS_CONF( 4, false,    HIGH, 2, 8192, 5500, 5000);
+	MBUS_CONF( 5, false,    HIGH, 2,  100,   64,   32);
+	MBUS_CONF( 6, false,    HIGH, 2,  100,   64,   32);
+	MBUS_CONF( 8, false,    HIGH, 0,  256,  128,   64);
+	MBUS_CONF(11, false,    HIGH, 0,  256,  128,  100);
+	MBUS_CONF(14, false,    HIGH, 0, 1024,  256,   64);
+	MBUS_CONF(16, false, HIGHEST, 6, 8192, 2800, 2400);
+	MBUS_CONF(21, false, HIGHEST, 6, 2048,  768,  512);
+	MBUS_CONF(22, false,    HIGH, 0,  256,  128,  100);
+	MBUS_CONF(25,  true, HIGHEST, 0,  100,   64,   32);
+	MBUS_CONF(26, false,    HIGH, 2, 8192, 5500, 5000);
+	MBUS_CONF(37, false,    HIGH, 0,  256,  128,   64);
+	MBUS_CONF(38, false,    HIGH, 2,  100,   64,   32);
+	MBUS_CONF(39, false,    HIGH, 2, 8192, 5500, 5000);
+	MBUS_CONF(40, false,    HIGH, 2,  100,   64,   32);
 
 	dmb();
 }
